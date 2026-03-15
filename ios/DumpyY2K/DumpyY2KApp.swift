802d06a3c47d0bf7bf07335d22156cb5e1071299
Ubuntu <ubuntu@ip-172-31-12-119.us-east-2.compute.internal>
2026-03-15 07:14:27 +0000

    Initialize analytics on app launch
    - TelemetryDeck + AppsFlyer initialized in App init()
    - Track app_launched event on appear
    - Start AppsFlyer SDK on appear

--- ios/DumpyY2K/DumpyY2KApp.swift
+++ ios/DumpyY2K/DumpyY2KApp.swift
@@ -2,10 +2,18 @@ import SwiftUI
 
 @main
 struct DumpyY2KApp: App {
+    init() {
+        AnalyticsService.shared.initialize()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.light)
+                .onAppear {
+                    AnalyticsService.shared.trackAppLaunched()
+                    AnalyticsService.shared.startAppsFlyer()
+                }
         }
     }
 }
