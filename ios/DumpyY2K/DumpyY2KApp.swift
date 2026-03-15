ee1ec651266bdbbc290b668e73ebc28528fc7bbf
Ubuntu <ubuntu@ip-172-31-12-119.us-east-2.compute.internal>
2026-03-15 03:23:41 +0000

    Force light keyboard appearance app-wide

--- ios/DumpyY2K/DumpyY2KApp.swift
+++ ios/DumpyY2K/DumpyY2KApp.swift
@@ -2,6 +2,11 @@ import SwiftUI
 
 @main
 struct DumpyY2KApp: App {
+    init() {
+        // Force light keyboard appearance
+        UITextField.appearance().keyboardAppearance = .light
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
